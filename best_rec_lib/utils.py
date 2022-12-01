a32c3e0e12c15e5d739660ee2afbf8d8a625391d
YulyaOfitserova <83347308+YulyaOfitserova@users.noreply.github.com>
2022-12-01 07:14:22 +0300

    Add files via upload

---
+++ best_rec_lib/utils.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+
+def prefilter_items(data, take_n_popular=5000, item_features=None):
+    popularity = data.groupby('item_id')['user_id'].nunique().reset_index() / data['user_id'].nunique()
+    popularity.rename(columns={'user_id': 'share_unique_users'}, inplace=True)
+
+    # Уберем самые популярные товары (их и так купят)
+    top_popular = popularity[popularity['share_unique_users'] > 0.2].item_id.tolist()
+    data = data[~data['item_id'].isin(top_popular)]
+
+    # Уберем самые НЕ популярные товары (их и так НЕ купят)
+    top_notpopular = popularity[popularity['share_unique_users'] < 0.02].item_id.tolist()
+    data = data[~data['item_id'].isin(top_notpopular)]
+
+    # Возьмем топ по популярности
+    popularity = data.groupby('item_id')['quantity'].sum().reset_index()
+    popularity.rename(columns={'quantity': 'n_sold'}, inplace=True)
+    top_n = popularity.sort_values('n_sold', ascending=False).head(take_n_popular).item_id.tolist()
+
+    # Заведем фиктивный item_id (если юзер покупал товары из топ-5000, то он "купил" такой товар)
+    data.loc[~data['item_id'].isin(top_n), 'item_id'] = 999999
+
+    return data
+
+def postfilter_items(user_id, recommednations):
+    pass
